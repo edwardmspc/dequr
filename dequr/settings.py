fae782198fe95e44ab94fe7c313f2ed4558c23ba
edwardmspc <edward.mspc@gmail.com>
2015-04-05 16:36:58 -0430

    correccion imagenes

--- dequr/settings.py
+++ dequr/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '$=#j301saw%gz+q@%xk0$4rqsk!b=0hqznd(@ma3t4_)qlunxu'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
