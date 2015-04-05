0e21836aea371be0ae87db0ee837449c4bfb55f6
edwardmspc <edward.mspc@gmail.com>
2015-04-05 16:04:57 -0430

    nueva funcionalidad

--- apps/general/urls.py
+++ apps/general/urls.py
@@ -5,3 +5,15 @@ from .views import IndexView
 urlpatterns = [
     url(r'^$', IndexView.as_view(), name='index'),
 ]
+
+
+from django.conf import settings
+if settings.DEBUG:
+    urlpatterns += [
+        url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root': settings.MEDIA_ROOT,
+        }),
+        url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root': settings.STATIC_ROOT,
+        }),
+    ]
