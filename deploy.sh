bbd9d53acacf794bf8be621161836daf1d5d82a7
edwardmspc <edward.mspc@gmail.com>
2015-04-05 20:23:17 -0430

    cambios al menu

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
-cd /home/dequr
-git pull origin master
+cd /home
+rm dequr -R
+mkdir dequr
+cd /dequr
+git init
+git clone git@github.com:edwardmspc/dequr.git
 sudo supervisorctl restart dequr
