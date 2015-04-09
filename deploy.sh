d6ce3bd6ed2de98ee5c714b1594dbff743acaece
edwardmspc <edward.mspc@gmail.com>
2015-04-08 22:34:43 -0430

    agregando single_page

--- deploy.sh
+++ deploy.sh
@@ -3,3 +3,8 @@ rm /home/dequr -R
 cd /home
 git clone git@github.com:edwardmspc/dequr.git
 sudo supervisorctl restart dequr
+
+
+rm dequr -R
+git clone git@github.com:edwardmspc/dequr.git
+sudo supervisorctl restart offergames
