4eac9fd4590c18086c2fe57e8de68b4fb8ad8875
edwardmspc <edward.mspc@gmail.com>
2015-04-10 12:16:25 -0430

    ultimos cambios en post_page

--- apps/complaint/urls.py
+++ apps/complaint/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import include, url
-from .views import IndexView
+from .views import IndexView, PostView
 
 
 urlpatterns = [
     url(r'^single_page/$', IndexView.as_view(), name='index'),
+    url(r'^post_page/$', PostView.as_view(), name='post_complaint'),
 ]
--- apps/complaint/views.py
+++ apps/complaint/views.py
@@ -5,3 +5,7 @@ from django.views.generic import View
 class IndexView(View):
     def get(self, request,  *args, **kwargs):
         return render(request, 'complaint/single_page.html')
+
+class PostView(View):
+    def get(self, request,  *args, **kwargs):
+        return render(request, 'complaint/post_page.html')
