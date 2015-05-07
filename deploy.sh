471c66d6759a7dd1a0bd0ec3d4f555be3ddd69de
Edd <edward.mspc@gmail.com>
2015-05-06 23:01:32 -0430

    probando hooks

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-rm /home/dequr -R
-cd /home
-git clone git@github.com:edwardmspc/dequr.git
-sudo supervisorctl restart dequr
+rm -r /home/dequr/webapps/dequr/dequr 
+cd /home/dequr/webapps/dequr
+git clone https://github.com/edwardmspc/dequr.git
+./apache2/bin/restart
